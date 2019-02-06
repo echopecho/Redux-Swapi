5a4cc4a9fda4b006034184bc8286ffa61449fad8
echopecho <pbsmithtbn@gmail.com>
2019-02-05 21:20:00 -0600

    Set up redux with redux-thunk
    Everything looks like it is set up correctly but for some reason the data will not display to screen. Pushing this commit to see if my PM can assist.

--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,13 +1,23 @@
-import /* we need our action types here*/ "../actions";
+import { FETCHING, SUCCESS, FAILURE } from "../actions";
+
 const initialState = {
-  characters: []
+  characters: [],
   // Array characters, Boolean fetching, null error.
+  fetching: false,
+  error: null
+
 };
 export const charsReducer = (state = initialState, action) => {
   switch (action.type) {
     // Fill me in with the important reducers
     // action types should be FETCHING, SUCCESS and FAILURE
     // your switch statement should handle all of these cases.
+    case FETCHING:
+      return { ...state, characters: [], fetching: true, error: "" }
+    case SUCCESS:
+      return { ...state, characters: action.payload, fetching: false, error: "" }
+    case FAILURE:
+      return { ...state, characters: [], fetching: false, error: 'Mayday'}
     default:
       return state;
   }
--- src/views/CharacterListView.js
+++ src/views/CharacterListView.js
@@ -3,33 +3,46 @@ import { connect } from "react-redux";
 
 import { CharacterList } from "../components";
 // import actions
+import { fetchCharacters } from '../actions';
 
 class CharacterListView extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
   }
 
   componentDidMount() {
     // call our action
+    this.props.fetchCharacters();
   }
 
   render() {
     if (this.props.fetching) {
       // return something here to indicate that you are fetching data
+      return (
+        <div>Loading....</div>
+      )
     }
-    return (
-      <div className="CharactersList_wrapper">
-        <CharacterList characters={this.props.characters} />
-      </div>
-    );
+      return (
+        <div className="CharactersList_wrapper">
+          <CharacterList characters={this.props.characters} />
+        </div>
+      );
+    
   }
 }
 
 // our mapStateToProps needs to have two properties inherited from state
 // the characters and the fetching boolean
+const mapStateToProps = state => {
+  return {
+    characters: state.characters,
+    fetching: state.fetching
+  }
+}
+
 export default connect(
-  null /* mapStateToProps replaces null here */,
+  mapStateToProps,
   {
-    /* action creators go here */
+    fetchCharacters
   }
 )(CharacterListView);
